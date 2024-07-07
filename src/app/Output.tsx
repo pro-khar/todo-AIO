a54f71efbace157f4f6bceb8b960739e66fa1e5d
pro-khar <prakharvermamanu.veed@gmail.com>
2024-07-08 00:07:31 +0530

    two entry todo with delete

--- src/app/Output.tsx
+++ src/app/Output.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./Redux/store";
+import { TrashIcon } from "@radix-ui/react-icons";
+import { removeTodo } from "./Redux/todoSlice";
 function Output() {
+  const dispatch = useDispatch();
   const todos = useSelector((state: RootState) => state.todos.todos);
   return (
     <div className="border rounded-md h-1/3 w-1/2 p-5">
@@ -9,6 +12,10 @@ function Output() {
         <div key={todo.id} className="flex justify-between">
           <p>{todo.title}</p>
           <p>{todo.gender}</p>
+          <TrashIcon
+            className=" text-red-500 w-5 h-5"
+            onClick={(e) => dispatch(removeTodo(todo.id))}
+          />
         </div>
       ))}
     </div>
